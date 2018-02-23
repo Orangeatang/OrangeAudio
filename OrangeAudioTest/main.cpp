157d8255faf056d942c49e212c15df1da02b43d4
Orangeatang <capoeiraolly@gmail.com>
2018-02-22 19:00:05 -0500

    fixed the solution so it actually runs on windows 8

---
+++ OrangeAudioTest/main.cpp
@@ -0,0 +1,53 @@
+
+//////////////////////////////////////////////////////////////////////////
+/// Includes
+//////////////////////////////////////////////////////////////////////////
+
+#include <OrangeAudioEngine.h>
+
+
+//////////////////////////////////////////////////////////////////////////
+/// Globals
+//////////////////////////////////////////////////////////////////////////
+
+static const OAInt64 g_defaultEmitter	= 0;
+static const OAInt64 g_defaultListener	= 0;
+
+
+//////////////////////////////////////////////////////////////////////////
+/// Application Entry Point
+//////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+	// create and initialize the audio engine
+	COrangeAudioEngine* audioEngine = new COrangeAudioEngine();
+	if( !audioEngine->Initialize() )
+	{
+		delete audioEngine;
+		return -1;
+	}
+
+	// register an emitter
+	if( !audioEngine->RegisterEmitter(g_defaultEmitter) )
+	{
+		delete audioEngine;
+		return -1;
+	}
+	
+	// register a listener
+	if( !audioEngine->RegisterListener(g_defaultListener) )
+	{
+		delete audioEngine;
+		return -1;
+	}
+
+
+	// pause in case we want to inspect the COrangeAudioEngine instance
+	// system( "pause" );
+
+	audioEngine->Uninitialize();
+	delete audioEngine;
+	
+	return 0;
+}
